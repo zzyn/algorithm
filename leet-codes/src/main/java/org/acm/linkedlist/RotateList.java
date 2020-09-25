bd9b75b05b3963c0c700af7eb7575c3eb0eb090a
Zack Zou <zack.zou@ef.com>
2020-09-25 16:03:27 +0800

    add rorate list

---
+++ leet-codes/src/main/java/org/acm/linkedlist/RotateList.java
@@ -0,0 +1,26 @@
+package org.acm.linkedlist;
+
+public class RotateList {
+
+    public ListNode rotateRight(ListNode head, int k){
+        if(head == null || k == 0) return head;
+        int len = 1;
+        ListNode p = head;
+        while (p.next != null) {
+            len++;
+            p = p.next;
+        }
+
+        k = len - k % 10;
+        //首尾相连
+        p.next = head;
+
+        for(int step = 0; step < k; step++){
+            p = p.next;
+        }
+
+        head = p.next; //新的首节点
+        p.next = null;//断开环
+        return head;
+    }
+}
