17c86a63667925adeb51ab9c0a3bf7f0c62e0d05
Zack Zou <zack.zou@ef.com>
2020-09-27 09:35:56 +0800

    remove nth from end list

---
+++ leet-codes/src/main/java/org/acm/linkedlist/RemoveNthFromEndList.java
@@ -0,0 +1,22 @@
+package org.acm.linkedlist;
+
+public class RemoveNthFromEndList {
+
+    public ListNode removeNthFromEnd(ListNode head, int n){
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode p = dummy, q = dummy;
+        for(int i = 0; i< n; i++){
+            q = q.next;
+        }
+
+        while (q.next != null) {
+            p = p.next;
+            q = q.next;
+        }
+
+        p.next = p.next.next;
+        return dummy.next;
+
+    }
+}
