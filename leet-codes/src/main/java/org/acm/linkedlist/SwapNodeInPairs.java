528d37296a4b32a3287cbb1723abb4fb68349cd6
Zack Zou <zack.zou@ef.com>
2020-09-28 13:46:48 +0800

    swap pairs

---
+++ leet-codes/src/main/java/org/acm/linkedlist/SwapNodeInPairs.java
@@ -0,0 +1,19 @@
+package org.acm.linkedlist;
+
+public class SwapNodeInPairs {
+
+    public  ListNode swapPairs(ListNode head){
+        if(head == null || head.next == null) return head;
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        for(ListNode prev = dummy, cur = prev.next, next = cur.next;
+            next != null;
+            prev = cur, cur = cur.next, next = cur != null ? cur.next: null
+            ) {
+            prev.next = next;
+            cur.next = next.next;
+            next.next = cur;
+        }
+        return dummy.next;
+    }
+}
