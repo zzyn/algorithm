2c769a6c182c1e3bd11b27541bddf623abde438b
Zack Zou <zack.zou@ef.com>
2020-09-28 16:36:28 +0800

    add detect cycle

--- leet-codes/src/main/java/org/acm/linkedlist/LinkedListCycle.java
+++ leet-codes/src/main/java/org/acm/linkedlist/LinkedListCycle.java
@@ -13,4 +13,25 @@ public class LinkedListCycle {
 
         return false;
     }
+
+    public ListNode detectCycle(ListNode head) {
+
+        ListNode slow = head, fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast) {
+                ListNode slow2 = head;
+                while (slow != slow2) {
+                    slow = slow.next;
+                    slow2 = slow2.next;
+                }
+
+                return slow2;
+            }
+        }
+
+        return null;
+    }
 }
