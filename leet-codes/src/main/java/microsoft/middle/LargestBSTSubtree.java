a5625f5e4a987c259f1983afe777f1f7faf536cf
zzyn <zzyn@github.com>
2021-03-13 22:02:25 +0800

    add binary tree upside down

--- leet-codes/src/main/java/microsoft/middle/LargestBSTSubtree.java
+++ leet-codes/src/main/java/microsoft/middle/LargestBSTSubtree.java
@@ -6,14 +6,11 @@ public class LargestBSTSubtree {
         int val;
         TreeNode left;
         TreeNode right;
-
         public TreeNode() {
         }
-
         public TreeNode(int val) {
             val = val;
         }
-
         public TreeNode(int val, TreeNode left, TreeNode right) {
             this.val = val;
             this.left = left;
