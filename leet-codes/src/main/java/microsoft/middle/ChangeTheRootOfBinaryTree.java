257e433ff8528fa86534c24f99515f06ca63a1d0
zzyn <zzyn@github.com>
2021-03-13 20:58:34 +0800

    add

---
+++ leet-codes/src/main/java/microsoft/middle/ChangeTheRootOfBinaryTree.java
@@ -0,0 +1,19 @@
+package microsoft.middle;
+
+public class ChangeTheRootOfBinaryTree {
+
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node parent;
+    }
+
+    public Node flipBinaryTree(Node root, Node leaf) {
+        return null;
+    }
+
+    public static void main(String[] args){
+
+    }
+}
