6d3a21cbf91f49ced7e5ef7247b9b0ef8c8460e4
zzyn <zzyn@github.com>
2021-03-15 20:03:37 +0800

    add

---
+++ leet-codes/src/main/java/leetcode/microsoft/middle/$666_PathSumIV.java
@@ -0,0 +1,40 @@
+package leetcode.microsoft.middle;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class $666_PathSumIV {
+
+    int ans = 0;
+    Map<Integer, Integer> values;
+    public int pathSum(int[] nums) {
+        values = new HashMap();
+        for (int num: nums)
+            values.put(num / 10, num % 10);
+
+        dfs(nums[0] / 10, 0);
+        return ans;
+    }
+
+    public void dfs(int node, int sum) {
+        if (!values.containsKey(node)) return;
+        sum += values.get(node);
+
+        int depth = node / 10, pos = node % 10;
+        int left = (depth + 1) * 10 + 2 * pos - 1;
+        int right = left + 1;
+
+        if (!values.containsKey(left) && !values.containsKey(right)) {
+            ans += sum;
+        } else {
+            dfs(left, sum);
+            dfs(right, sum);
+        }
+    }
+
+
+    public static void main(String[] args){
+        System.out.println(new $666_PathSumIV().pathSum(new int[]{113, 215, 221}));
+    }
+}
